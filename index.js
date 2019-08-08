fe02272e925c314ac59ec597ab9425c2284d4823
Ferit Tunçer <ferit@cryptolab.net>
2019-08-09 01:44:20 +0300

    test build

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const csv = require('csv')
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.get('/', async function(req, res) {
-  const address = '0x988b3a538b618c7a603e1c11ab82cd16dbe28069'
-  const rows = await klerosLiquidSetStake(address)
+// app.get('/', async function(req, res) {
+//   const address = '0x988b3a538b618c7a603e1c11ab82cd16dbe28069'
+//   const rows = await klerosLiquidSetStake(address)
+//
+//   const date = new Date()
+//
+//   res.setHeader(
+//     'Content-disposition',
+//     `attachment; filename=kleros-liquid-set-stake-${address}-${date.toISOString()}.csv`
+//   )
+//   res.writeHead(200, {
+//     'Content-Type': 'text/csv'
+//   })
+//
+//   csv.stringify(rows).pipe(res)
+//   // res.send(filePath) // Set disposition and send it.
+// })
 
-  const date = new Date()
-
-  res.setHeader(
-    'Content-disposition',
-    `attachment; filename=kleros-liquid-set-stake-${address}-${date.toISOString()}.csv`
-  )
-  res.writeHead(200, {
-    'Content-Type': 'text/csv'
-  })
-
-  csv.stringify(rows).pipe(res)
-  // res.send(filePath) // Set disposition and send it.
-})
+app.get('/', (req, res) => res.send('Hello World!'))
